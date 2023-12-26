b95901c091fc54b0abb49422c613b43ebae32843
PauloAlves8039 <pj38alves@gmail.com>
2023-12-25 21:43:11 -0300

    Configurando serviços e injeção de dependência

--- src/Estacionamento.WebAPI/Program.cs
+++ src/Estacionamento.WebAPI/Program.cs
@@ -1,3 +1,11 @@
+using Estacionamento.Data.Context;
+using Estacionamento.Data.Interfaces;
+using Estacionamento.Data.Repositories;
+using Estacionamento.Service.Interfaces;
+using Estacionamento.Service.Mappings;
+using Estacionamento.Service.Services;
+using Microsoft.EntityFrameworkCore;
+
 namespace Estacionamento.WebAPI
 {
     public class Program
@@ -6,21 +14,46 @@ namespace Estacionamento.WebAPI
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+            builder.Services.AddAutoMapper(typeof(EstacionamentoProfile));
+
+            builder.Services.AddDbContext<EstacionamentoContext>((services, options) =>
+            {
+                var configuration = services.GetRequiredService<IConfiguration>();
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            builder.Services.AddScoped<IClienteRepository, ClienteRepository>();
+            builder.Services.AddScoped<IClienteService, ClienteService>();
+
+            builder.Services.AddScoped<IClienteVeiculoRepository, ClienteVeiculoRepository>();
+            builder.Services.AddScoped<IClienteVeiculoService, ClienteVeiculoService>();
+
+            builder.Services.AddScoped<IEnderecoRepository, EnderecoRepository>();
+            builder.Services.AddScoped<IEnderecoService, EnderecoService>();
+
+            builder.Services.AddScoped<IVeiculoRepository, VeiculoRepository>();
+            builder.Services.AddScoped<IVeiculoService, VeiculoService>();
+
+            builder.Services.AddScoped<IPermanenciaService, PermanenciaService>();
+            builder.Services.AddScoped<IPermanenciaRepository, PermanenciaRepository>();
+
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
+
+            app.UseCors(options =>
+            {
+                options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+            });
 
             app.UseHttpsRedirection();
 
