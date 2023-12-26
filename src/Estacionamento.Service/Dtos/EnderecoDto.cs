d18f31669207b587bfcf5d67b8c83b51c6c9bb51
PauloAlves8039 <pj38alves@gmail.com>
2023-12-25 23:26:10 -0300

    Adicionando propriedade no EnderecoDto

--- src/Estacionamento.Service/Dtos/EnderecoDto.cs
+++ src/Estacionamento.Service/Dtos/EnderecoDto.cs
@@ -7,6 +7,7 @@
         public string Numero { get; set; }
         public string? Complemento { get; set; }
         public string Bairro { get; set; }
+        public string Cidade { get; set; }
         public string Estado { get; set; }
         public string Cep { get; set; }
     }
